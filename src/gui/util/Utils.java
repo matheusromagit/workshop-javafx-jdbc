70dea7a787d8e43eb56a8c20364a96e983d085ed
Matheus Roma <matheusroma.ctt@gmail.com>
2025-01-07 09:06:24 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -9,4 +9,9 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+
+	public static Integer tryParseToInt(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
